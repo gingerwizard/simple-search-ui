2872661d4c0854173ea4cfa740855142aae99313
Dale McDiarmid <dalem@elastic.co>
2017-09-28 13:50:52 +0100

    backup image

--- client/app/components/results/SearchHit.js
+++ client/app/components/results/SearchHit.js
@@ -3,12 +3,16 @@ var PropTypes = require('prop-types');
 import {
   KuiText
 } from '../../ui_framework/components';
+import Img from 'react-image'
+import noImage from './no_image.png'
 
-
+//style={{'backgroundImage': `url("${}")`}}
 function SearchHit (props) {
     return (
       <KuiText className="search-hit">
-          <div className="search-hit-image" style={{'backgroundImage': `url("${props.hit.get('img')}")`}}/>
+          <div className="search-hit-image">
+            <Img src={[props.hit.get('img'),noImage]}/>
+          </div>
           <div className="search-image-details">
             <h3>{props.hit.get('title')}</h3>
 
